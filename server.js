5b69314dcded8c5f49a7be39efe6576a4e160c8e
machetazo <fabianmontero1998@gmail.com>
2017-01-14 20:36:46 +0000

    improved database

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var mqtt = require('mqtt');
 var config = require('./config.json');
-//var database = require('./services/database');
+var database = require('./services/database');
 
 var broker = config.broker + ':' + config.port;
 
-console.log(config.serverName + " Server Status: \u001B[41mOFFLINE\u001B[0m\nBooting...");
+console.log(config.serverName + " Server Status: \u001B[31mOFFLINE\u001B[0m\nBooting...");
 
 
 var server = mqtt.connect(broker);
@@ -16,11 +16,11 @@ server.on('connect' , function(){
 
 	server.publish('imaginexyz/server/status' , config.serverName + ' Server Status: ONLINE');
 
-	console.log(config.serverName + ' Server Status: \u001B[42mONLINE\u001B[0m');
+	console.log(config.serverName + ' Server Status: \u001B[32mONLINE\u001B[0m');
 	
 	server.subscribe('#' , function(){
 	
-		console.log(config.serverName + ' Server Status: \u001B[44mWAITING\u001B[0m on all topics (\u001B[33m#\u001B[0m)');
+		console.log(config.serverName + ' Server Status: \u001B[34mWAITING\u001B[0m on all topics (\u001B[33m#\u001B[0m)');
 	
 		server.on('message' , function(topic , message , packet){
 
--- services/database.js
+++ services/database.js
@@ -1,2 +1,16 @@
 var mongo = require('mongodb');
+var db;
+
+
+mongo.MongoClient.connect('mongodb://localhost:27017/iot' , function(err , database) {
+    if(!err) {
+        db = database; //Instancia de la base de datos
+        
+        console.log('.\n.\n.\n.\n"iot" Database Status: \u001B[32mACTIVE\u001B[0m \n');
+    }
+    else{
+        console.log(404, '\u001B[31mError Connecting to the "iot" database\u001B[0m');
+    }
+});
+
 
